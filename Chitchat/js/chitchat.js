520cf77a0ed953fb17f23d1fb72e2c5a0979fa3f
Moumita Sen <sen.moumita@gmail.com>
2013-07-10 13:21:33 -0700

    updates

---
+++ Chitchat/js/chitchat.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+		$('a.trial_class_button').click(function(){
+    	$('a.trial_class_button')
+    	$('body').css('background-color','#f00')
+    	$('a.trial_class_button').colorbox({href:"trial.html"});
+  })
+
+})
+
